f84c875f144b57eb123eb3786e0172a2a4eeb2b6
Ngatere12 <60994382+Ngatere12@users.noreply.github.com>
2020-02-20 13:25:06 +1300

    Update Game.py
    Tidy up Code

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 import sys
 
-playerA = str(input("Player A, please enter your name "))
-playerB = str(input("Player B, please enter your name "))
+playerA = str(input("Player A, please enter your name: "))
+playerB = str(input("Player B, please enter your name: "))
 
 def rockpaperscissors():
     powerplayerA = str(input(playerA + " please choose your power: Rock, Paper or Scissors? "))
@@ -11,19 +11,19 @@ def rockpaperscissors():
         print("Its a tie!")
     elif powerplayerA == "Rock":
         if powerplayerB == "Scissors":
-            print(playerA + "wins!")
+            print(playerA + " wins!")
         else:
-            print(playerB + "wins!")
+            print(playerB + " wins!")
     elif powerplayerA == "Paper":
         if powerplayerB == "Rock":
-            print(playerA + "wins!")
+            print(playerA + " wins!")
         else:
-            print(playerB + "wins!")
+            print(playerB + " wins!")
     elif powerplayerA == "Scissors":
         if powerplayerB == "Paper":
-            print(playerA + "wins!")
+            print(playerA + " wins!")
         else:
-            print(playerB + "wins!")
+            print(playerB + " wins!")
 
     playagain()
 
